81212b82976e7ff60fa7d3d4e174e2c1bf71eba2
alexfriedl <anecronika@gmx.de>
2019-11-24 10:05:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-learn-anything
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-learn-anything)
---
+++ index.js
@@ -0,0 +1,51 @@
+var initialPromiseCount = 0;
+
+// Wir erstellen einen Button
+// Button triggered Promise
+var button = document.getElementById("action");
+button.addEventListener("click", function() {
+  testPromise();
+});
+
+function testPromise() {
+  // Wir loggen einen Counter im DOM
+  var promiseCount = ++initialPromiseCount;
+  var log = document.getElementById("log");
+
+  // PROMISE STARTED (1)
+  // Wir erstellen ein Promise Objekt
+  var promise1 = new Promise(
+    // Die Resolver-Funktion kann den Promise auflösen oder verwerfen
+    function(resolve, reject) {
+      log.insertAdjacentHTML(
+        "beforeend",
+        promiseCount +
+          ") PROMISE STARTED (1) / <small>Async code started</small><br/>"
+      );
+      // Asynchron
+      setTimeout(function() {
+        // We fulfill the promise !
+        resolve(promiseCount);
+      }, Math.random() * 1500);
+    }
+  );
+
+  // PROMISE FULFILLED (3)
+  promise1.then(
+    // Logging via HTML
+    function(result) {
+      log.insertAdjacentHTML(
+        "beforeend",
+        result +
+          ") PROMISE FULFILLED (3) / <small>Async code terminated</small><br/>"
+      );
+    }
+  );
+
+  // PROMISE MADE (2)
+  log.insertAdjacentHTML(
+    "beforeend",
+    promiseCount +
+      ") PROMISE MADE (2) / <small>Sync code terminated</small><br/>"
+  );
+}
